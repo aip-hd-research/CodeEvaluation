f7f81ccb1e22f5f39a824a0012d18060134f21fe
LeStoe11 <lennart@stoepler.de>
2025-03-19 15:35:26 +0100

    feat(structure): Add dummy files

---
+++ src/codeevaluation/execution/Executor.py
@@ -0,0 +1,8 @@
+from codeevaluation.typing import BagOfProperties
+
+class Executor:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    print("blah")
---
+++ tests/test_codeevaluation/execution/test_Executor.py
@@ -0,0 +1,4 @@
+from codeevaluation.execution.Executor import Executor
+
+def test_Executor():
+    executor = Executor()
