5f8ae2a31dee8603de52689d9c71c481369660c8
LeStoe11 <lennart@stoepler.de>
2025-03-24 18:36:25 +0100

    fix(setup): Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -i
 
-ENV_NAME="${1:-'CodeEvaluation'}"
+ENV_NAME="${1:-"CodeEvaluation"}"
 conda env create -y -n $ENV_NAME -f environment.dev.yaml
 conda activate $ENV_NAME
-bash build_and_test.sh
-conda env update -y -n $ENV_NAME -f environment.test.yaml
+chmod +x build_and_test.sh
+./build_and_test.sh
+conda env update -n $ENV_NAME -f environment.test.yaml
 pre-commit install
-conda deactivate $ENV_NAME
+conda deactivate
