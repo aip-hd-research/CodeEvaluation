83ef2cafe37cbc410fa78b97c38d3bce542b365c
Janik7777 <janikschmid@t-online.de>
2025-03-30 14:11:37 +0000

    feat(typing): add BoPcolumn typing

--- src/codeevaluation/typing/column_types.py
+++ src/codeevaluation/typing/column_types.py
@@ -1,13 +1,15 @@
+from typing import Type
+
 from codeevaluation.typing.BagOfProperties import BoPColumn
 
 # Universal usage
-ID = BoPColumn.create("id", str)
+ID: Type[BoPColumn] = BoPColumn.create("id", str)
 
 # For D test data
-DTranslations = BoPColumn.create("d_translations", str)
-DWithParams = BoPColumn.create("d_with_params", str)
-DExecutable = BoPColumn.create("d_executable", str)
+DTranslations: Type[BoPColumn] = BoPColumn.create("d_translations", str)
+DWithParams: Type[BoPColumn] = BoPColumn.create("d_with_params", str)
+DExecutable: Type[BoPColumn] = BoPColumn.create("d_executable", str)
 
 # For execution results
-Success = BoPColumn.create("success", bool)
-Error = BoPColumn.create("error", str)
+Success: Type[BoPColumn] = BoPColumn.create("success", bool)
+Error: Type[BoPColumn] = BoPColumn.create("error", str)
